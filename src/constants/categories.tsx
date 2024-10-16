a17fb69ed3e063d03015203f162538369d4181ee
amin23877 <amin23877@gmail.com>
2024-10-16 11:56:51 +0330

    categories enum

---
+++ src/constants/categories.tsx
@@ -0,0 +1,33 @@
+import MusicNoteRoundedIcon from "@mui/icons-material/MusicNoteRounded";
+import VideocamRoundedIcon from "@mui/icons-material/VideocamRounded";
+import DisplaySettingsRoundedIcon from "@mui/icons-material/DisplaySettingsRounded";
+import SportsEsportsRoundedIcon from "@mui/icons-material/SportsEsportsRounded";
+import MovieFilterRoundedIcon from "@mui/icons-material/MovieFilterRounded";
+
+const categories = [
+    {
+        id: 1,
+        title: "audio",
+        icon: <MusicNoteRoundedIcon />,
+    },
+    {
+        id: 2,
+        title: "video",
+        icon: <VideocamRoundedIcon />,
+    },
+    {
+        id: 3,
+        title: "app",
+        icon: <DisplaySettingsRoundedIcon />,
+    },
+    {
+        id: 4,
+        title: "game",
+        icon: <SportsEsportsRoundedIcon />,
+    },
+    {
+        id: 5,
+        title: "porn",
+        icon: <MovieFilterRoundedIcon />,
+    },
+];
